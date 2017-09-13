4b77d81a2eb91be0ecf5fb797516b38cf6980af0
rcammisola <rocco.cammisola@gmail.com>
2017-09-13 23:10:26 +0100

    First test added

---
+++ tests/BirthdayServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace test\Rocco;
+
+use PHPUnit\Framework\TestCase;
+// use Rocco\BirthdayService;
+
+class BirthdayServiceTest extends TestCase
+{
+    public function testPhpUnitRunsATest()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
